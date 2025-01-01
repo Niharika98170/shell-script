c395f3fdadc87895a7705a687915fe4d31e3cc3e
Niharika98170 <niharikaashinaveni98@gmail.com>
2025-01-01 21:35:52 +0530

    conversation added

--- 01-arrays.sh
+++ 01-arrays.sh
@@ -4,7 +4,7 @@ MOVIES=("devara" "rrr" "Pushpa")
 #index starts from 0, size is 3
 
 echo "First movie: ${MOVIES[0]}"
-echo "Second movie: ${MOVIES{[1]}"
+echo "Second movie: ${MOVIES[1]}"
 echo "Third movie: ${MOVIES[2]}"
 
 echo "All movies are: ${Movies[@]}"
