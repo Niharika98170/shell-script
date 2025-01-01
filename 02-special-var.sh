f12969eda200423ae8a6e7b0ce0d1c831dd00699
Niharika98170 <niharikaashinaveni98@gmail.com>
2025-01-01 21:47:04 +0530

    conversation added

---
+++ 02-special-var.sh
@@ -0,0 +1,10 @@
+#!/bash/bin
+
+echo "All variables are passed: $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directorty of current user: $HOME"
+echo "Which user is running the script: $USER"
+echo "Procees id of current script: $$"
+echo "Process id of lasr command in background: $!"
